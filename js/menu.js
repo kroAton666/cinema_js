b0fe7b0af6ad970fd722538b3e7a81e18fce99c0
kroAton666 <maxjuck00@gmail.com>
2021-12-08 15:25:31 +0200

    day 10

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+const open_menu = (nav, active) =>{
+    nav.classList.add(active)
+}
+
+const closeMenu = (nav, active) => {
+    nav.classList.remove(active)
+}
+const slideMenu = ({openBtn, menu, classActiveMenu, closeTrigger}) => {
+    const burgerBtn = document.querySelector(openBtn);
+    const navigation = document.querySelector(menu);
+    const navigationClose = document.querySelectorAll(closeTrigger);
+    burgerBtn.addEventListener('click', () =>{
+        open_menu(navigation, classActiveMenu)
+    })
+    navigationClose.forEach(item =>{
+        item.addEventListener('click', () => {
+            closeMenu(navigation, classActiveMenu)
+        })
+    })
+}
+export default slideMenu
