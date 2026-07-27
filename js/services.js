a0a5dd26556ed551f21c4bff3af4ce16f08dcccd
kroAton666 <68004521+kroAton666@users.noreply.github.com>
2026-07-27 15:41:57 +0300

    Update services.js

--- js/services.js
+++ js/services.js
@@ -1,6 +1,6 @@
 const API_KEY = '8e77f18670c85ae0619e40d29b8258a5';
 const BASE_URL = 'https://api.themoviedb.org/3/';
-const LANGUAGE = '&language=ru-RU';
+const LANGUAGE = '&language=ua-UK';
 
 //trending/all/day?api_key=<<api_key>>
 
